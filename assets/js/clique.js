0126bacbf3b50340571edc573a5d0fdfc560d404
eduardawzorek <eduarda_mw_silva@estudante.sc.senai.br>
2022-11-28 10:15:11 -0300

    opa

---
+++ assets/js/clique.js
@@ -0,0 +1,36 @@
+// const pop = document.querySelector('div.pop')
+// const popup = document.querySelector('.popup-wrapper')
+// const closepop = document.querySelector('.popup-close')
+// let imagem = document.getElementById('one')
+// let imagens = ["https://www.granmarquise.com.br/wp-content/uploads/2018/02/1-2.png", "https://www.granmarquise.com.br/wp-content/uploads/2018/02/2-3.png", "https://cf.bstatic.com/xdata/images/hotel/max1024x768/37714480.jpg?k=9e001c13161aaa23f78efafccd894aeaa006db49b1000153e7f36536d6122e95&o="]
+// let cont = 0
+
+// pop.addEventListener('click',() => {
+//     popup.style.display = 'block'
+// })
+
+// closepop.addEventListener('click',() => {
+//     popup.style.display = 'none'
+// })
+
+// function mudar() {
+//     for(cont == 0; cont < 2; cont++) {
+//         imagem.src = imagens[cont++]
+//     }
+//     console.log(cont)
+// }
+
+// function retornar() {
+//     imagens.src = imagens[cont--]
+//     console.log(cont)
+// }
+
+
+
+
+
+
+
+
+
+
---
+++ exclusivo.php
@@ -0,0 +1,10 @@
+<?php
+    
+    $dbHost = 'LocalHost' ;
+    $dbUsername = 'root' ;
+    $dbPassword = '';
+    $dbName = 'exclusivo';
+
+    $conexao = new msqli( $dbHost,$dbUsername,$dbPassword,$dbName)
+
+?>
